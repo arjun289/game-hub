3cff146f4ec702506491f9664f438495fa2eb12c
singh16 <singh16@imec.be>
2023-09-14 23:37:26 +0100

    feat: add game card skeleton

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow="hidden">
+    <Card width="300px" borderRadius={10} overflow="hidden">
       <Image src={getCroppedIMageUrl(game.background_image)}></Image>{" "}
       <CardBody>
         {" "}
