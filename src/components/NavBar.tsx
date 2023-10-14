cf6ce9b8c0edf242e05f5fb67dfe6a00ad04d499
singh16 <arjun289singh@gmail.com>
2023-10-14 21:04:50 +0100

    feat: add search feature

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,17 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import { SearchInput } from "./SearchInput";
 
-const NavBar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack paddingBottom={2}>
       <Image src={logo} boxSize="60px"></Image>
-      <SearchInput></SearchInput>
+      <SearchInput onSearch={onSearch}></SearchInput>
       <ColorModeSwitch></ColorModeSwitch>
     </HStack>
   );
